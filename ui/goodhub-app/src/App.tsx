d6673dc9a159d34a46bfc37a40e2ab8d31b4080d
James Williams <james@projecthoneycomb.org>
2021-02-26 16:02:47 +0000

    Removed temp dependancy on goodhub-shared

--- ui/goodhub-app/src/App.tsx
+++ ui/goodhub-app/src/App.tsx
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Skeleton } from 'goodhub-shared';
-
 function App() {
   return (
     <div className="App">
@@ -17,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Skeleton width={1000}></Skeleton>
         </a>
       </header>
     </div>
