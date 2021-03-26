3af16b90a6ffdd4bbd8859ea20188150f2905cda
James Williams <jr_williams@me.com>
2021-03-26 18:08:47 +0000

    Disable main body scroll when modal is open

--- ui/goodhub-app/src/components/generic/Modal.tsx
+++ ui/goodhub-app/src/components/generic/Modal.tsx
@@ -1,5 +1,5 @@
 import { Transition } from '@headlessui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type ModalProps = {
   state: ModalState
@@ -14,8 +14,15 @@ export enum ModalState {
   Closed
 }
 
-
 const Modal: React.FC<ModalProps> = ({ state, children, onDismiss, layout = 'items-center', className = '', padding = 'px-4 pt-5 pb-4 sm:p-6' }) => {
+
+  useEffect(() => {
+    if (state === ModalState.Open) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [state]);
 
   return <Transition
     show={state === ModalState.Open}
@@ -27,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({ state, children, onDismiss, layout = 'ite
     leaveTo="transform opacity-0"
     as={React.Fragment}
   >
-    <div className={`fixed h-screen w-screen flex inset-0 p-0 justify-center ${layout} z-50`}>
+    <div className={`fixed h-screen w-screen overflow-scroll flex inset-0 p-0 justify-center ${layout} z-50`}>
       <div onClick={onDismiss} className="fixed inset-0 transition-opacity">
         <div className="absolute inset-0 bg-black opacity-75 pointer-events-none"></div>
       </div>
