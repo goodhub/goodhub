223099d82b7dff85a9add8c42d3c3180f281961c
James Williams <jr_williams@me.com>
2021-04-25 15:59:20 +0100

    Added netlify plugins to smooth build

---
+++ ui/goodhub-app/plugins/notify-slack/index.js
@@ -0,0 +1,53 @@
+const https = require('https');
+
+const sendToSlack = (message) => {
+  const data = JSON.stringify({
+    text: message
+  })
+
+  const options = {
+    hostname: 'hooks.slack.com',
+    port: 443,
+    path: '/services/T01TQESD09H/B01V5U2TTU6/1HydXxdFVPKVxr8EZ4ZUL7b9',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+
+    const req = https.request(options, res => {
+      console.log(`statusCode: ${res.statusCode}`)
+    
+      res.on('data', d => {
+        process.stdout.write(d)
+      })
+
+      res.on('end', () => resolve())
+    })
+    
+    req.on('error', error => {
+      reject(error);
+    })
+    
+    req.write(data)
+    req.end()
+  })
+}
+
+module.exports = {
+  onPreBuild: async ({ constants }) => {
+    const { SITE_ID } = constants;
+    await sendToSlack(`<https://app.netlify.com/sites/${SITE_ID}/deploys/${process.env.BUILD_ID}|A new UI build has started for "${process.env.SITE_NAME}" with target "${process.env.REACT_APP_BUILD_TARGET}">.`)
+  },
+  onError: async ({ constants }) => {
+    const { SITE_ID } = constants;
+    await sendToSlack(`<https://app.netlify.com/sites/${SITE_ID}/deploys/${process.env.BUILD_ID}|Failed build & deploy for "${process.env.SITE_NAME}">`)
+  },
+  onSuccess: async ({ constants }) => {
+    const { SITE_ID } = constants;
+    await sendToSlack(`<https://app.netlify.com/sites/${SITE_ID}/deploys/${process.env.BUILD_ID}|Successful build & deploy for "${process.env.SITE_NAME}">\nSee live site here -> <${process.env.URL}>`)
+  }
+}
---
+++ ui/goodhub-app/plugins/upgrade-lib/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  onPreBuild: async ({ utils: { run }}) => {
+    console.log('Upgrading @strawberrylemonade/goodhub-lib to the latest version');
+    await run.command('npm i @strawberrylemonade/goodhub-lib@latest');
+  },
+}
