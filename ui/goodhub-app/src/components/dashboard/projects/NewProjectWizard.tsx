f4f57ddf4df893733a92df77a43990ac5102c11c
James Williams <jr_williams@me.com>
2021-05-19 21:35:09 +0100

    New project wizard

---
+++ ui/goodhub-app/src/components/dashboard/projects/NewProjectWizard.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import Modal, { ModalState } from '../../generic/Modal';
+import Wizard from '../../generic/Wizard';
+// import { Step } from 'react-albus';
+// import Title from '../../generic/Title';
+import { FormProvider, useForm } from 'react-hook-form';
+import { IProject } from '@strawberrylemonade/goodhub-lib';
+import OrganisationDecoration from '../../decoration/OrganisationDecoration';
+import { useErrorService } from '../../../services/error-service';
+import { createProject, useOrganisationService } from '../../../services/organisation-service';
+import { useHistory } from 'react-router';
+import { useNotificationService } from '../../../services/notification-service';
+
+export interface NewProjectWizardProps {
+  modalState: ModalState
+  onDismiss: () => void
+}
+
+const NewProjectWizard: FC<NewProjectWizardProps> = ({ modalState, onDismiss }) => {
+
+  const organisation = useOrganisationService(state => state.organisation);
+  const setError = useErrorService(state => state.setError);
+  const addNotification = useNotificationService(state => state.addNotification)
+  const methods = useForm<Partial<IProject>>({ shouldUnregister: false });
+  const history = useHistory();
+
+  const submit = async (data: Partial<IProject>) => {
+    if (!organisation) return;
+
+    try {
+      const project = await createProject(organisation.id, data);
+      addNotification('Project was created successfully.')
+      history.push(`/dashboard/${organisation.id}/projects/${project.id}`);
+    } catch (e) {
+      setError(e)
+      onDismiss();
+    }
+  }
+
+  return <Modal padding="p-0" layout="items-center" state={modalState} onDismiss={onDismiss}>
+    <FormProvider {...methods}>
+      <form>
+        <Wizard
+          name="Create an organisation"
+          decoration={(className) => <OrganisationDecoration className={className} />}
+          introduction={<p>From registered charities and non-profits to individual community projects, you can sign up for an organisation on GoodHub and get started making a difference.</p>}
+          onComplete={methods.handleSubmit(submit)}
+        >
+          <div></div>
+        </Wizard>
+      </form>
+    </FormProvider>
+  </Modal>
+}
+
+export default NewProjectWizard;
