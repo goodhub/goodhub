a8efedd170b3a5fe337ec95848e75b7d6e35da1d
James Williams <jr_williams@me.com>
2021-07-20 12:34:21 +0100

    Fixed graphic to image function & UI plugin for getting latest library

--- packages/functions/graphic-to-image/index.ts
+++ packages/functions/graphic-to-image/index.ts
@@ -4,6 +4,12 @@ import { Dir } from 'temporary';
 import { v4 } from 'uuid';
 import { UploadImage, ProcessedImage, Status } from '../upload-image';
 
+declare global {
+  interface Window {
+    __k_freeze: boolean;
+  }
+}
+
 const tempDir = new Dir();
 
 const GraphicToImage: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
@@ -22,13 +28,23 @@ const GraphicToImage: AzureFunction = async function (context: Context, req: Htt
     const url = req.body.url || "http://localhost:3000";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.goto(`${url}/graphics?config=${encodeURIComponent(JSON.stringify(req.body.config))}`);
+    await page.waitForSelector('.graphic-container');
+
     await page.setViewport({
-      width: 600,
-      height: 314,
+      width: req.body.size.width,
+      height: req.body.size.height,
       deviceScaleFactor: 2
     });
-    await page.goto(`${url}/graphics?config=${encodeURIComponent(JSON.stringify(req.body.config))}`);
-    await page.waitForTimeout(1000)
+    await page.evaluate(() => {
+      return new Promise((resolve) => {
+        window.requestAnimationFrame(resolve)
+      })
+    })
+    await page.waitForTimeout(500);
+    await page.evaluate(() => {
+      window.__k_freeze = true
+    })
     await page.screenshot({ fullPage: true, path: image.location.path });
     await browser.close();
     const result = await UploadImage(context, null, image);
--- packages/ui/plugins/upgrade-lib/index.js
+++ packages/ui/plugins/upgrade-lib/index.js
@@ -2,5 +2,6 @@ module.exports = {
   onPreBuild: async ({ utils: { run }}) => {
     console.log('Upgrading @strawberrylemonade/goodhub-lib to the latest version');
     await run.command('npm i @strawberrylemonade/goodhub-lib@latest');
+    await run.command('npm i @strawberrylemonade/kyan@latest');
   },
 }
