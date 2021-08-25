a2afb2ab136feff38c5f2285eb7369924c0c365f
Mark Williams <mark.futurlix@gmail.com>
2021-08-25 11:21:40 +0100

    change for justification of menu items

--- packages/ui/src/components/generic/Action.tsx
+++ packages/ui/src/components/generic/Action.tsx
@@ -9,7 +9,7 @@ export const Action: FC<ActionProps> = ({ type, active, children }) => {
   switch (type) {
     case 'card':
     default:
-      return <button className={`flex items-center text-gray-700 dark:text-white text-sm px-3 py-2 text rounded-md ${active ? 'font-medium' : ''} hover:font-medium hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:text-gray-900 cursor-pointer`}>
+      return <button className={`flex items-center text-gray-700 text-left dark:text-white text-sm px-3 py-2 text rounded-md ${active ? 'font-medium' : ''} hover:font-medium hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:text-gray-900 cursor-pointer`}>
       { children }
     </button>
   };
