2e0da8358541b6b3facc5ac115dc2330f1d202b2
James Williams <jr_williams@me.com>
2022-02-20 20:33:33 +0000

    Updated netlify plugins

--- packages/ui/plugins/upgrade-lib/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  onPreBuild: async ({ utils: { run }}) => {
-    console.log('Upgrading @strawberrylemonade/goodhub-lib to the latest version');
-    await run.command('npm i @strawberrylemonade/goodhub-lib@latest');
-    await run.command('npm i @strawberrylemonade/kyan@latest');
-  },
-}
